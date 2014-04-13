62620856375cb9bdde5acd0c58af59c820fdaea1
Uwe@WS08 <uhw@gmx.org>
2014-04-13 15:48:45 +0200

    Start.

---
+++ MyAppAutoReStart/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyAppAutoReStart
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public App()
+        {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        /// <summary>
+        /// OnDispatcherUnhandledException
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string ErrorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+            Debug.WriteLine(ErrorMessage);
+            e.Handled = true;
+            ApplicationReStart();
+        }
+
+        /// <summary>
+        /// ApplicationReStart
+        /// </summary>
+        void ApplicationReStart()
+        {
+            string ErrorMessage = string.Format("The application has encountered a problem and needs to be restarted!");
+            MessageBox.Show(ErrorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ MyAppAutoReStart/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿/******************************************************************************/
+/*                                                                            */
+/*   Program: MyAppAutoReStart                                                */
+/*   How to restart an application, by itself.                                */
+/*                                                                            */
+/*   07.04.2014 0.0.0.0 uhwgmxorg Start                                       */
+/*                                                                            */
+/******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAppAutoReStart
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// button_ThrowExeption_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_ThrowExeption_Click(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Fatal Error!");
+        }
+    }
+}
